7b87b6ca4fac5cc2d434b5adc2b639a75e416ad6
Capn <38225872+CapnS@users.noreply.github.com>
2019-10-23 14:16:39 -0500

    Update email_validator.py

--- email_validator.py
+++ email_validator.py
@@ -13,13 +13,13 @@ def valid(email):
         print("There is no . after the @.")
         return False
     second, third = second.split(".")
-    if not len(first):
+    if not first:
         print("The part of the email before @ is blank.")
         return False
-    elif not len(second):
+    elif not second:
         print("The part of the email between @ and . is blank.")
         return False
-    elif not len(third):
+    elif not third:
         print("The part of the email after . is blank.")
         return False
     print("Valid email address.")
